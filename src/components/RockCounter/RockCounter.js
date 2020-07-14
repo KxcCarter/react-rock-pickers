11832e5dd8a4e79f213f9fd9d8fcca97640234a4
Kenneth Carter <kxccarter@gmail.com>
2020-07-14 14:37:32 -0500

    RockCounter.js base functionality added.

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 
 class RockCounter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rocks: 0,
+    };
+  }
+
+  increase = () => {
+    this.setState({
+      rocks: this.state.rocks + 1,
+    });
+  };
+
+  decrease = () => {
+    this.setState({
+      rocks: this.state.rocks - 1,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      rocks: 0,
+    });
+  };
+
   render() {
     return (
       <div>
-          <div>
-              Rocks Picked: 0
-          </div>
-          <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
-          </div>
+        <div>Rocks Picked: {this.state.rocks}</div>
+        <div>
+          <button onClick={this.increase}>Increase</button>
+          <button onClick={this.decrease}>Decrease</button>
+          <button onClick={this.reset}>Reset</button>
+        </div>
       </div>
     );
   }
